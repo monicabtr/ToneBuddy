cf08767666931f70452cc829cce98697c6629f3b
Monica Butarbutar <monicabutarbutar04@gmail.com>
2023-08-11 14:45:47 +0700

    Initial Commit

---
+++ ToneBuddy/ToneBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneBuddyApp.swift
+//  ToneBuddy
+//
+//  Created by Monica Butarbutar on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ToneBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
